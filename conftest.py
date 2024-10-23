0d0e19f45a36cdc1ebe891c28db27f134fd83797
Pfliger Konstantin <pfliger.kostya@yandex.ru>
2024-10-23 15:01:02 +0300

    correction of comments

--- conftest.py
+++ conftest.py
@@ -9,7 +9,4 @@ def collector():
     return collector
 
 
-@pytest.fixture
-def books_genre(collector):
-    collector.add_new_book('Гордость и предубеждение и зомби')
-    return collector
+
--- tests.py
+++ tests.py
@@ -10,7 +10,8 @@ class TestBooksCollector:
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         assert len(collector.get_books_genre()) == 2
 
-    def test_add_new_book_book_added_without_genre(self, collector, books_genre):
+    def test_add_new_book_book_added_without_genre(self, collector):
+        collector.add_new_book('Гордость и предубеждение и зомби')
         assert collector.get_book_genre('Гордость и предубеждение и зомби') == ''
 
     @pytest.mark.parametrize('book_name', ['', 'К' * 41])
@@ -18,16 +19,19 @@ class TestBooksCollector:
         collector.add_new_book(book_name)
         assert len(collector.get_books_genre()) == 0
 
-    def test_add_new_book_duplicate_book_not_added(self, collector, books_genre):
+    def test_add_new_book_duplicate_book_not_added(self, collector):
         collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book(str(collector.books_genre.keys()))
         assert len(collector.get_books_genre()) == 1
 
     @pytest.mark.parametrize('genre', ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии'])
-    def test_set_book_genre_existing_genre_added(self, collector, books_genre, genre):
+    def test_set_book_genre_existing_genre_added(self, collector, genre):
+        collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_genre('Гордость и предубеждение и зомби', genre)
         assert collector.get_book_genre('Гордость и предубеждение и зомби') == genre
 
-    def test_set_book_genre_not_existing_genre_not_added(self, collector, books_genre):
+    def test_set_book_genre_not_existing_genre_not_added(self, collector):
+        collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_genre('Гордость и предубеждение и зомби', 'Мюзикл')
         assert collector.get_book_genre('Гордость и предубеждение и зомби') == ''
 
@@ -35,33 +39,40 @@ class TestBooksCollector:
         collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
         assert collector.get_book_genre('Гордость и предубеждение и зомби') is None
 
-    def test_get_book_genre_genre_for_existing_book(self, collector, books_genre):
+    def test_get_book_genre_genre_for_existing_book(self, collector):
+        collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
         assert collector.get_book_genre('Гордость и предубеждение и зомби') == 'Ужасы'
 
     def test_get_book_genre_genre_from_not_existing_book(self, collector):
-        assert collector.get_book_genre('Гордость и предубеждение и зомби') is None
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        assert collector.get_book_genre('Отсутствующая книга') is None
 
     @pytest.mark.parametrize('genre', ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии'])
-    def test_get_books_with_specific_genre_get_books_for_existing_in_books_genre(self, collector, books_genre, genre):
+    def test_get_books_with_specific_genre_get_books_for_existing_in_books_genre(self, collector, genre):
+        collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_genre('Гордость и предубеждение и зомби', genre)
         assert collector.get_books_with_specific_genre(genre) == ['Гордость и предубеждение и зомби']
 
-    def test_get_books_with_specific_genre_get_books_for_not_existing_in_books_genre(self, collector, books_genre):
+    def test_get_books_with_specific_genre_get_books_for_not_existing_in_books_genre(self, collector):
+        collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
         assert collector.get_books_with_specific_genre('Мюзикл') == []
 
     @pytest.mark.parametrize('age_rating', ['Ужасы', 'Детективы'])
-    def test_get_books_for_children_no_book_with_age_rating(self, collector, books_genre, age_rating):
+    def test_get_books_for_children_no_book_with_age_rating(self, collector, age_rating):
+        collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_genre('Гордость и предубеждение и зомби', age_rating)
         assert collector.get_books_for_children() == []
 
     @pytest.mark.parametrize('age_rating', ['Фантастика', 'Мультфильмы', 'Комедии'])
-    def test_get_books_for_children_get_book_without_age_rating(self, collector, books_genre, age_rating):
+    def test_get_books_for_children_get_book_without_age_rating(self, collector, age_rating):
+        collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_genre('Гордость и предубеждение и зомби', age_rating)
         assert collector.get_books_for_children() == ['Гордость и предубеждение и зомби']
 
-    def test_add_book_in_favorites_existing_book_added(self, collector, books_genre):
+    def test_add_book_in_favorites_existing_book_added(self, collector):
+        collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_book_in_favorites('Гордость и предубеждение и зомби')
         assert collector.get_list_of_favorites_books() == ['Гордость и предубеждение и зомби']
 
@@ -69,17 +80,20 @@ class TestBooksCollector:
         collector.add_book_in_favorites('Гордость и предубеждение и зомби')
         assert collector.get_list_of_favorites_books() == []
 
-    def test_add_book_in_favorites_duplicate_book_not_added(self, collector, books_genre):
+    def test_add_book_in_favorites_duplicate_book_not_added(self, collector):
+        collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_book_in_favorites('Гордость и предубеждение и зомби')
         collector.add_book_in_favorites('Гордость и предубеждение и зомби')
         assert len(collector.get_list_of_favorites_books()) == 1
 
     def test_delete_book_from_favorites_existing_book_delete(self, collector):
+        collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_book_in_favorites('Гордость и предубеждение и зомби')
         collector.delete_book_from_favorites('Гордость и предубеждение и зомби')
         assert collector.get_list_of_favorites_books() == []
 
-    def test_delete_book_from_favorites_not_existing_book_not_delete(self, collector, books_genre):
+    def test_delete_book_from_favorites_not_existing_book_not_delete(self, collector):
+        collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_book_in_favorites('Гордость и предубеждение и зомби')
         collector.delete_book_from_favorites('Что делать, если ваш кот хочет вас убить')
         assert collector.get_list_of_favorites_books() == ['Гордость и предубеждение и зомби']
